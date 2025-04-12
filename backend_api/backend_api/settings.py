9fb9b00b5551acfbed3c099599915934771e3959
Aditya <adityagiri2204@gmail.com>
2025-04-12 22:32:56 +0530

    changed pagination class from LimitOffsetPagination to PageNumberPagination

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -131,7 +131,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
         ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE': 1
+     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 1
     
 }
