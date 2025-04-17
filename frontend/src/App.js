673bad354fca89a7435c6024917535145ddf0c08
Aditya <adityagiri2204@gmail.com>
2025-04-17 23:40:06 +0530

    started frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="container">
+        <h1>this is sample app in react.js</h1>      
+    </div>
+  );
+}
+
+export default App;
