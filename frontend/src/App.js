f9a169823a6b90a117ea41e0d7eeddc3d1fb933a
Aditya <adityagiri2204@gmail.com>
2025-04-23 22:31:08 +0530

    made customer review design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,18 +8,18 @@ import Header from "./components/Headers";
 function App() {
   return (
     <>
-     
-     <Header />
+
+      <Header />
       <main className="mt-3">
-{/* latest product  */}
+        {/* latest product  */}
         <div className="container">
           <h3 className="fw-bold mb-4">
             Latest Products <a href="#"
-              className="float-end btn btn-dark">View All Products  
-              <i class="fa-solid fa-arrow-right-long ms-1"></i></a>
+              className="float-end btn btn-dark">View All Products
+              <i className="fa-solid fa-arrow-right-long ms-1"></i></a>
           </h3>
           <div className="row mb-4">
-           
+
             {/* produt box */}
             <div className="col-12 col-md-3 mb-4">
               <div className="card" style={{ width: "18rem" }}>
@@ -120,8 +120,8 @@ function App() {
               </div>
             </div>
             {/* produt box end */}
-          {/* produt box */}
-          <div className="col-12 col-md-3 mb-4">
+            {/* produt box */}
+            <div className="col-12 col-md-3 mb-4">
               <div className="card" style={{ width: "18rem" }}>
                 <img className="card-img-top" src={logo} alt="Card cap" />
                 <div className="card-body">
@@ -221,19 +221,19 @@ function App() {
             </div>
             {/* produt box end */}
           </div>
-        {/* latest product end  */}
+          {/* latest product end  */}
         </div>
 
 
-{/* Popular Categories*/}
+        {/* Popular Categories*/}
         <div className="container">
           <h3 className="fw-bold mb-4">
             Popular Categories <a href="#"
-              className="float-end btn btn-dark">View All Categories  
-              <i class="fa-solid fa-arrow-right-long ms-1"></i></a>
+              className="float-end btn btn-dark">View All Categories
+              <i className="fa-solid fa-arrow-right-long ms-1"></i></a>
           </h3>
           <div className="row mb-4">
-           
+
             {/* Category box */}
             <div className="col-12 col-md-3 mb-4">
               <div className="card" style={{ width: "18rem" }}>
@@ -241,13 +241,13 @@ function App() {
                 <div className="card-body">
                   <h4 className="card-title">Product Category</h4>
                   <div className="card-footer">
-                      product download : 598
-                </div>
+                    product download : 598
+                  </div>
                 </div>
               </div>
             </div>
             {/* Category box end */}
-            
+
             {/* Category box */}
             <div className="col-12 col-md-3 mb-4">
               <div className="card" style={{ width: "18rem" }}>
@@ -255,13 +255,13 @@ function App() {
                 <div className="card-body">
                   <h4 className="card-title">Product Category</h4>
                   <div className="card-footer">
-                      product download : 598
-                </div>
+                    product download : 598
+                  </div>
                 </div>
               </div>
             </div>
             {/* Category box end */}
-            
+
             {/* Category box */}
             <div className="col-12 col-md-3 mb-4">
               <div className="card" style={{ width: "18rem" }}>
@@ -269,13 +269,13 @@ function App() {
                 <div className="card-body">
                   <h4 className="card-title">Product Category</h4>
                   <div className="card-footer">
-                      product download : 598
-                </div>
+                    product download : 598
+                  </div>
                 </div>
               </div>
             </div>
             {/* Category box end */}
-            
+
             {/* Category box */}
             <div className="col-12 col-md-3 mb-4">
               <div className="card" style={{ width: "18rem" }}>
@@ -283,27 +283,27 @@ function App() {
                 <div className="card-body">
                   <h4 className="card-title">Product Category</h4>
                   <div className="card-footer">
-                      product download : 598
-                </div>
+                    product download : 598
+                  </div>
                 </div>
               </div>
             </div>
             {/* Category box end */}
           </div>
-        {/* Popular Categoy product end  */}
+          {/* Popular Categoy product end  */}
         </div>
 
 
-{/* Popular product  */}
+        {/* Popular product  */}
 
-      <div className="container">
+        <div className="container">
           <h3 className="fw-bold mb-4">
             Popular Products <a href="#"
-              className="float-end btn btn-dark">View All Products  
-              <i class="fa-solid fa-arrow-right-long ms-1"></i></a>
+              className="float-end btn btn-dark">View All Products
+              <i className="fa-solid fa-arrow-right-long ms-1"></i></a>
           </h3>
           <div className="row mb-4">
-           
+
             {/* Popular Product box */}
             <div className="col-12 col-md-3 mb-4">
               <div className="card" style={{ width: "18rem" }}>
@@ -311,21 +311,21 @@ function App() {
                 <div className="card-body">
                   <h4 className="card-title">Product Title</h4>
                   <div className="card-footer">
-                      Price: RS.500
-                </div>
+                    Price: RS.500
+                  </div>
                 </div>
               </div>
             </div>
             {/* Popular Product box end */}
-                        {/* Popular Product box */}
-                        <div className="col-12 col-md-3 mb-4">
+            {/* Popular Product box */}
+            <div className="col-12 col-md-3 mb-4">
               <div className="card" style={{ width: "18rem" }}>
                 <img className="card-img-top" src={logo} alt="Card cap" />
                 <div className="card-body">
                   <h4 className="card-title">Product Title</h4>
                   <div className="card-footer">
-                      Price: RS.500
-                </div>
+                    Price: RS.500
+                  </div>
                 </div>
               </div>
             </div>
@@ -336,8 +336,8 @@ function App() {
                 <div className="card-body">
                   <h4 className="card-title">Product Title</h4>
                   <div className="card-footer">
-                      Price: RS.500
-                </div>
+                    Price: RS.500
+                  </div>
                 </div>
               </div>
             </div>
@@ -348,75 +348,128 @@ function App() {
                 <div className="card-body">
                   <h4 className="card-title">Product Title</h4>
                   <div className="card-footer">
-                      Price: RS.500
-                </div>
+                    Price: RS.500
+                  </div>
                 </div>
               </div>
             </div>
             {/* Popular Product box end */}
-            
 
-            
-            
+
+
+
           </div>
-        {/* Popular product end  */}
-            </div>
+          {/* Popular product end  */}
+        </div>
 
 
-{/* popular seller */}
-<div className="container">
+        {/* popular seller */}
+        <div className="container">
           <h3 className="fw-bold mb-4">
             Popular Seller <a href="#"
               className="float-end btn btn-dark">View All Seller
-              <i class="fa-solid fa-arrow-right-long ms-1"></i></a>
+              <i className="fa-solid fa-arrow-right-long ms-1"></i></a>
           </h3>
           <div className="row mb-4">
-       
-            {/* Popular Seller box*/}            
+
+            {/* Popular Seller box*/}
             <div className="col-12 col-md-3 mb-4">
               <div className="card" style={{ width: "18rem" }}>
                 <img className="card-img-top" src={logo} alt="Card cap" />
                 <div className="card-body">
                   <h4 className="card-title">Seller Name</h4>
                   <div className="card-footer">
-                     Categories: <a href="#">Python</a>, <a href="#">DJango</a>
-                </div>
+                    Categories: <a href="#">Python</a>, <a href="#">DJango</a>
+                  </div>
                 </div>
               </div>
             </div>
             {/* Popular Seller box end */}
-            {/* Popular Seller box*/}            
+            {/* Popular Seller box*/}
             <div className="col-12 col-md-3 mb-4">
               <div className="card" style={{ width: "18rem" }}>
                 <img className="card-img-top" src={logo} alt="Card cap" />
                 <div className="card-body">
                   <h4 className="card-title">Seller Name</h4>
                   <div className="card-footer">
-                     Categories: <a href="#">Fastapi</a>, <a href="#">Flask</a>
-                </div>
+                    Categories: <a href="#">Fastapi</a>, <a href="#">Flask</a>
+                  </div>
                 </div>
               </div>
             </div>
             {/* Popular Seller box end */}
-            {/* Popular Seller box*/}            
+            {/* Popular Seller box*/}
             <div className="col-12 col-md-3 mb-4">
               <div className="card" style={{ width: "18rem" }}>
                 <img className="card-img-top" src={logo} alt="Card cap" />
                 <div className="card-body">
                   <h4 className="card-title">Seller Name</h4>
                   <div className="card-footer">
-                     Categories: <a href="#">React</a>, <a href="#">Node</a>
-                </div>
+                    Categories: <a href="#">React</a>, <a href="#">Node</a>
+                  </div>
                 </div>
               </div>
             </div>
             {/* Popular Seller box end */}
 
           </div>
-        {/* Popular Seller end  */}
-            </div>
+          {/* Popular Seller end  */}
+        </div>
 
-
+        {/* customer rating and reviews */}
+        <div id="carouselExampleIndicators" className="carousel slide mb-5 mx-auto custom-width bg-dark text-white p-5">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <figure className="text-center">
+                <blockquote className="blockquote">
+                  <p>A well-known quote, contained in a blockquote element.</p>
+                </blockquote>
+                <figcaption className="blockquote-footer">
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i><cite title="Source Title">Source Title</cite>
+                </figcaption>
+              </figure>    </div>
+            <div className="carousel-item">
+              <figure className="text-center">
+                <blockquote className="blockquote">
+                  <p>first comment by aditya ji .</p>
+                </blockquote>
+                <figcaption className="blockquote-footer">
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i> <cite title="Source Title">Source Title</cite>
+                </figcaption>
+              </figure>    </div>
+            <div className="carousel-item">
+              <figure className="text-center">
+                <blockquote className="blockquote">
+                  <p>osho the master of spirituality and philosophy</p>
+                </blockquote>
+                <figcaption className="blockquote-footer">
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i><cite title="Source Title">Source Title</cite>
+                </figcaption>
+              </figure>    </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </main>
     </>
   );
