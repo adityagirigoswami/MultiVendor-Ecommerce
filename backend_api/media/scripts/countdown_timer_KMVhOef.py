a47701a322331b8664fb9d51b2c2ced27567fb45
Aditya <adityagiri2204@gmail.com>
2025-07-01 03:50:47 +0530

    resolve containerization issue

---
+++ backend_api/media/scripts/countdown_timer_KMVhOef.py
@@ -0,0 +1,12 @@
+import time
+
+def countdown(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        print(f"{mins:02d}:{secs:02d}", end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
+
+# Example usage:
+countdown(10)
